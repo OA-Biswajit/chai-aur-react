46c3c9c592c4af89eac2d75f38eff2bfcf6f186a
Biswajit kumar senapati <biswajitkumarsenapati2003@gmail.com>
2023-10-04 01:03:01 +0530

    video 21

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
-
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import authSlice, { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import {Outlet} from "react-router-dom"
 function App() {
-// console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">      
+      <Header />
+      <main>
+        {/* Todo outlet */}
+        TODO:
+        <Outlet />
+      </main>
+      <Footer />
+
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
